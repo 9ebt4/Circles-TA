dfcb4526a93bf5a165c56e0e3d08c120bc1de44f
ethan <ethanbjansen@gmail.com>
2023-07-19 12:16:18 -0500

    first

---
+++ Circle Tester TA/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Object
+{
+    public class Circle
+    {
+        //Properties
+        private double Radius { get; set; }
+
+        //Constructor
+        public Circle(double _radius) 
+        {
+            Radius= _radius;
+        }
+
+        public double CalculateDiameter()
+        {
+            return Radius * 2;
+        }
+        public double CalculateCircumference()
+        {
+            return Radius * 2 * Math.PI;
+        }
+        public double CalculateArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+        public void Grow()
+        {
+            Radius = Radius* 2;
+        }
+        public double GetRadius()
+        { 
+            return Radius;
+        }
+         
+    }
+}
---
+++ Circle Tester TA/Program.cs
@@ -0,0 +1,33 @@
+﻿using Circle_Object;
+using Validator_Object;
+
+
+bool runProgram = true;
+while (runProgram)
+{
+    Console.WriteLine("Enter a radius.");
+    Circle newCircle = new Circle(Validator.DoubleValidator());
+    Console.WriteLine($"Diameter:{newCircle.CalculateDiameter()}");
+    Console.WriteLine($"Circumference:{newCircle.CalculateCircumference()}");
+    Console.WriteLine($"Area:{newCircle.CalculateArea()}");
+
+    while (true)
+    {
+        Console.WriteLine("Make it grow?");
+        bool response = Validator.Continue();
+        if(response)
+        {
+            newCircle.Grow();
+            Console.WriteLine($"Diameter:{newCircle.CalculateDiameter()}");
+            Console.WriteLine($"Circumference:{newCircle.CalculateCircumference()}");
+            Console.WriteLine($"Area:{newCircle.CalculateArea()}");
+        }
+        else
+        {
+            Console.WriteLine("GoodBye");
+            Console.WriteLine($"Ending Radius: {newCircle.GetRadius()}");
+            runProgram = response; 
+            break;
+        }
+    }
+}
